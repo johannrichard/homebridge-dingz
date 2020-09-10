b57317c7d1b4cff8f1eaff2bf58c161271b8aa9b
Johann Richard <189003+johannrichard@users.noreply.github.com>
2020-09-10 16:20:53 +0200

    fix(platform): fix typo, revert to old style

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { API } from 'homebridge';
+import type { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
 import { PLUGIN_NAME } from './settings';
@@ -7,6 +7,6 @@ import { DingzDaHomebridgePlatform } from './platform';
 /**
  * This method registers the platform with Homebridge
  */
-export default function(api: API) {
-  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, DingzDaHomebridgePlatform);
-}
+export = (api: API) => {
+  api.registerPlatform(PLATFORM_NAME, DingzDaHomebridgePlatform);
+};
