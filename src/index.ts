ea3a7a026f60d34141491e20d06be5a8e930b64b
Johann Richard <189003+johannrichard@users.noreply.github.com>
2020-09-10 15:50:25 +0200

    fix(platform): fix for #56
    - Expand platform registration to include plugin name besides platform name (See #56 and also https://github.com/homebridge/homebridge-plugin-template/issues/11)

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
-import type { API } from 'homebridge';
+import { API } from 'homebridge';
 
 import { PLATFORM_NAME } from './settings';
+import { PLUGIN_NAME } from './settings';
 import { DingzDaHomebridgePlatform } from './platform';
 
 /**
  * This method registers the platform with Homebridge
  */
-export = (api: API) => {
-  api.registerPlatform(PLATFORM_NAME, DingzDaHomebridgePlatform);
+export default function(api: API) {
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, DingzDaHomebridgePlatform);
 };
