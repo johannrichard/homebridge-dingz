dfb84c0ca6b9c909fa7b911e7ecce4db94231c86
Johann Richard <189003+johannrichard@users.noreply.github.com>
2020-09-10 15:51:33 +0200

    fix(platform): implement fix for #56

--- src/settings.ts
+++ src/settings.ts
@@ -2,6 +2,7 @@
  * This is the name of the platform that users will use to register the plugin in the Homebridge config.json
  */
 export const PLATFORM_NAME = 'Dingz';
+export const PLUGIN_NAME = 'homebridge-dingz';
 export const ACCESSORY_NAME = 'Da';
 export const DINGZ_DISCOVERY_PORT = 7979;
 export const DINGZ_CALLBACK_PORT = 18081;
